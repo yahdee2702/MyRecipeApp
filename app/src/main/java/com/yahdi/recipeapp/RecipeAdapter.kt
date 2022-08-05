50433984f41051aa203faf534411a4d8b7417d3f
Yahdi_2702 <yahdi2702@gmail.com>
2022-08-05 13:55:05 +0700

    Changed adapter to be more easier to understand.

--- app/src/main/java/com/yahdi/recipeapp/RecipeAdapter.kt
+++ app/src/main/java/com/yahdi/recipeapp/RecipeAdapter.kt
@@ -8,38 +8,32 @@ import com.bumptech.glide.Glide
 import com.yahdi.recipeapp.databinding.ItemRecipesBinding
 
 class RecipeAdapter(private val recipes: ArrayList<RecipeData>): RecyclerView.Adapter<RecipeAdapter.MyViewHolder>() {
-    class MyViewHolder(private val binding: ItemRecipesBinding): RecyclerView.ViewHolder(binding.root) {
-        companion object {
-            fun from(parent: ViewGroup): MyViewHolder {
-                val layoutInflater = LayoutInflater.from(parent.context)
+    class MyViewHolder(val binding: ItemRecipesBinding): RecyclerView.ViewHolder(binding.root)
 
-                return MyViewHolder(
-                    ItemRecipesBinding.inflate(layoutInflater, parent, false)
-                )
-            }
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
 
-        fun bind(data: RecipeData) {
-            binding.apply {
-                tvTitle.text = data.title
-
-                Glide.with(root.context)
-                    .load(data.imageUrl)
-                    .into(ivPreview)
-
-                root.setOnClickListener {
-                    val intent = Intent(root.context, DetailActivity::class.java)
-                    intent.putExtra(DetailActivity.RECIPE_DATA, data)
-                    root.context.startActivity(intent)
-                }
-            }
-        }
+        return MyViewHolder(
+            ItemRecipesBinding.inflate(layoutInflater, parent, false)
+        )
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MyViewHolder.from(parent)
-
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.bind(recipes[position])
+        val data = recipes[position]
+
+        holder.binding.apply {
+            tvTitle.text = data.title
+
+            Glide.with(root.context)
+                .load(data.imageUrl)
+                .into(ivPreview)
+
+            root.setOnClickListener {
+                val intent = Intent(root.context, DetailActivity::class.java)
+                intent.putExtra(DetailActivity.RECIPE_DATA, data)
+                root.context.startActivity(intent)
+            }
+        }
     }
 
     override fun getItemCount() = recipes.size
